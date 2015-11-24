9c01221f79eb23964cc9b9b5f5635f0df99ec419
Martin DeMello <mdemello@google.com>
2015-11-23 21:44:57 -0800

    mouse and keyboard handling

--- gtkgui.ml
+++ gtkgui.ml
@@ -1,5 +1,6 @@
 open StdLabels
 open Types
+open Cursor
 
 let check_cache ~cond ~create ~destroy = function
     Some pm ->
@@ -27,13 +28,86 @@ class xw_widget ~xw ?packing ?show () =
   let height = xw.rows * scale in
   let da = GMisc.drawing_area ~width ~height ?packing ?show () in
   let context = da#misc#create_pango_context in
+  let point_to_cell x y =
+    let f u = int_of_float u / scale in
+    (f x, f y)
+  in
   object (self)
     inherit GObj.widget_full da#as_widget
     val rows = xw.rows
     val cols = xw.cols
     val mutable cursor = Cursor.make xw.rows xw.cols
+    val mutable dir : [`Across | `Down] = `Across
     val mutable size = 0, 0
     val mutable pixmap = None
+
+    initializer
+      da#event#connect#expose ~callback:(fun _ -> self#draw; true);
+      da#event#connect#key_press
+        ~callback:(fun ev -> self#handle_key_press ev);
+
+      da#event#add [`BUTTON_PRESS];
+      da#event#connect#button_press
+        ~callback:(fun ev -> self#handle_button_press ev);
+
+      da#misc#set_can_focus true;
+      da#misc#grab_focus ();
+      ()
+
+    method toggle_dir =
+      dir <- match dir with `Across -> `Down | `Down -> `Across
+
+    method toggle_black =
+      if Xword.toggle_black xw cursor.x cursor.y then begin
+        Xword.renumber xw |> ignore;
+        self#draw
+      end
+
+    method set_letter c =
+      let s = Char.uppercase c |> String.make 1 in
+      Xword.set_cell xw cursor.x cursor.y (Letter s);
+      self#move_cursor ~wrap:false (dir :> direction)
+
+    method move_cursor ?wrap:(wrap=true) (d : direction) =
+      let new_cursor = Cursor.move cursor ~wrap:wrap d in
+      self#set_cursor new_cursor
+
+    method set_cursor new_cursor =
+      cursor <- new_cursor;
+      self#draw
+
+    method handle_button_press ev =
+      let handled = ref true in
+      let _ = match GdkEvent.Button.button ev with
+      | 1 -> (
+          let x = GdkEvent.Button.x ev in
+          let y = GdkEvent.Button.y ev in
+          let x, y = point_to_cell x y in
+          self#set_cursor {cursor with x; y}
+        )
+      | _ -> handled := false
+      in
+      !handled
+
+    method handle_key_press ev =
+      let is_letter k = (Char.code 'a') <= k && k <= (Char.code 'z') in
+      let handled = ref true in
+      let _ = match GdkEvent.Key.keyval ev with
+      | k when k = GdkKeysyms._Left -> self#move_cursor `Left
+      | k when k = GdkKeysyms._KP_Left -> self#move_cursor `Left
+      | k when k = GdkKeysyms._Right -> self#move_cursor `Right
+      | k when k = GdkKeysyms._KP_Right -> self#move_cursor `Right
+      | k when k = GdkKeysyms._Up -> self#move_cursor `Up
+      | k when k = GdkKeysyms._KP_Up -> self#move_cursor `Up
+      | k when k = GdkKeysyms._Down -> self#move_cursor `Down
+      | k when k = GdkKeysyms._KP_Down -> self#move_cursor `Down
+      | k when k = GdkKeysyms._space -> self#toggle_black
+      | k when k = GdkKeysyms._Page_Up -> self#toggle_dir
+      | k when is_letter k -> self#set_letter (Char.chr k)
+      | _ -> handled := false
+      in
+      !handled
+
     method draw =
       let {Gtk.x=x0; y=y0; width=width; height=height} =
         da#misc#allocation in
@@ -92,9 +166,6 @@ class xw_widget ~xw ?packing ?show () =
       done;
 
       (new GDraw.drawable da#misc#window)#put_pixmap ~x:0 ~y:0 dr#pixmap
-
-    initializer
-      da#event#connect#expose ~callback:(fun _ -> self#draw; true); ()
   end
 
 let () =
