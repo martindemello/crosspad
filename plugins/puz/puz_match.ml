d99acdef61f3a592039b43ed276f62dbc2b8decf
Martin DeMello <mdemello@google.com>
2015-12-13 17:25:38 -0800

    puz mikmatch file

---
+++ plugins/puz/puz_match.ml
@@ -0,0 +1,19 @@
+open Core_kernel.Std
+open Puz_types
+open Mikmatch
+
+RE ltim = bos (digit+ as x : int) "," (digit+ as y : int) '\000' eos;;
+
+RE rtbl_entry = (" " digit | digit{2}) ":" alpha+ ";"
+RE rtbl = bos rtbl_entry+ '\000' eos
+RE rtbl_capture = (digit+ as symbol : int) ":" (alpha+ as word)
+
+let match_ltim s = match s with
+  | RE ltim -> Some (x, y)
+  | _ -> None
+
+let match_rtbl s =
+  let scan = COLLECTOBJ rtbl_capture in
+  match s with
+  | RE rtbl -> Some (scan s)
+  | _ -> None
