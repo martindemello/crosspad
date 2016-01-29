15a83aa3b93845784c4729011760fa3534ff5d08
Martin DeMello <mdemello@google.com>
2016-01-28 19:43:27 -0800

    add mit license

--- plugins/aclite/puz_match.ml
+++ plugins/aclite/puz_match.ml
@@ -8,6 +8,8 @@ RE rtbl_entry = (" " digit | digit{2}) ":" alpha+ ";"
 RE rtbl = bos rtbl_entry+ '\000' eos
 RE rtbl_capture = (digit+ as symbol : int) ":" (alpha+ as word)
 
+RE text_section_header = "<" (alpha+ as word) ">"
+
 let match_ltim s = match s with
   | RE ltim -> Some (x, y)
   | _ -> None
@@ -16,4 +18,8 @@ let match_rtbl s =
   let scan = COLLECTOBJ rtbl_capture in
   match s with
   | RE rtbl -> Some (scan s)
+  | _ -> None
+
+let match_text_section s = match s with
+  | RE text_section_header -> Some word
   | _ -> None
